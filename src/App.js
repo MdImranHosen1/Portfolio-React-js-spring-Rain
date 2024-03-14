bd34cd01792e030453d52b324b5aec9155e4d8c5
MdImranHosen1 <www.imransk.com@gmail.com>
2024-03-15 00:12:36 +0600

    "Introduction path fix"

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
           <Route path='project' element={<AllProject />} />
           <Route path='recommended' element={<Recommend />} />
           <Route path='contact' element={<Contact />} />
-          <Route path='*' element={<Experience />} />
+          <Route path='*' element={<Introduction />} />
 
         </Routes>
 
